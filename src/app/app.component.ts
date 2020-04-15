e6f3a65cca2272b3794dc94e238658628b5efbf7
nicho2605 <c14180046@john.petra.ac.id>
2020-04-15 10:24:29 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,21 @@ export class AppComponent  {
   b;
   output = "";
   hasil;
+  besar;
 
   klikhasil(angka1,angka2){
     this.a = parseInt(angka1);
     this.b = parseInt(angka2);
     this.hasil = Math.pow(parseInt(angka1), parseInt(angka2));
+    this.besar = this.a;
+    if(this.b>this.a){
+      this.besar = this.b;
+    }
     this.output = 'hasil =  '+ this.hasil + '<br/>';
 
     if (this.hasil % 2 ==0){
       this.output += "Pola B <br/>"
-        for(let i = this.b ;i<=this.hasil; i++){
+        for(let i = this.besar ;i<=this.hasil; i++){
           for(let j = 0; j<i; j++){
             this.output += "*"
           }
@@ -41,14 +46,14 @@ export class AppComponent  {
         }
         else{
           for(let i = 0; i<this.a; i++){
-        for(let j = this.b-i; j>0; j--){
-          this.output += "*"
-        }
-        for(let x = 0; x<i+1 && x<this.a && i<this.a; x++){
-          this.output += "O"
-        }
-        this.output += "<br>"
-      }
+            for(let j = this.b-i; j>0; j--){
+              this.output += "*"
+            }
+            for(let x = 0; x<i+1 && x<this.a && i<this.a; x++){
+              this.output += "O"
+            }
+            this.output += "<br>"
+          }
         }
       }
     }
